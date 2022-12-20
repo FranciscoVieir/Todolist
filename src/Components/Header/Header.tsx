752851d36b3252a58f4a1b22a389a55f57939584
FranciscoVieir <franciscovieira6397@gmail.com>
2022-12-20 16:16:15 -0300

    feat: structuring the header and removing lint errors due to import errors

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,12 +1,24 @@
+import rocketLogo from '../../assets/rocket.svg'
+import todoLogo from '../../assets/todo.svg'
+
+import styles from './Header.module.css';
+
 export default function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className={styles['container-header']}>
+      <img
+        src={rocketLogo}
+        alt="Rocket Logo"
+        className={styles['rocket-logo']}
+      />
       <div>
-        <img src="" alt="" />
-      </div>
 
-      <img src="" alt="" />
+        <img
+          src={todoLogo}
+          alt="Logo todolist"
+          className={styles['todo-logo']}
+        />
+      </div>
     </header>
   )
 }
