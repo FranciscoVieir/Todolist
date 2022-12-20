6960310742b5618f9043bdfd1f32a4a52382fd9b
FranciscoVieir <franciscovieira6397@gmail.com>
2022-12-20 14:08:58 -0300

    feat: creating new components and standardizing the project

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+      <div>
+        <img src="" alt="" />
+      </div>
+
+      <img src="" alt="" />
+    </header>
+  )
+}
