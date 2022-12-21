92f404e3781e3946f52bf79914877197ca995d15
FranciscoVieir <franciscovieira6397@gmail.com>
2022-12-21 17:23:24 -0300

    feat: creating a task menu structure and adding CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from './Components/Header'
+import TaskMenu from './Components/TaskMenu'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <TaskMenu />
     </div>
   )
 }
